5deefb5765763f83f428210d15b928ec4b79a713
emckean <>
2023-06-30 16:45:37 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # drupal-transfer
 View template for Drupal 7 along with a python script to clean the output
+Template configuration may differ per site. 
+- Views -> import to import template.
+
+Python3 is required for `script.py`
---
+++ script.py
@@ -0,0 +1,36 @@
+import re
+# Read the input file
+with open('export.xml', 'r') as file:
+    input = file.read()
+
+site = "caps"
+
+# Clean the HREFs
+regex = r'href="https:\/\/' + site + r'\.umich\.edu(.*?)"'
+substitution = r'href="\1"'
+modified_input = re.sub(regex, substitution, input)
+
+# Clean the Image tags
+regex_image = r'<Image>.*src="(.*?)".*</Image>'
+substitution = r'<Image>\1</Image>'
+modified_input = re.sub(regex_image, substitution, modified_input)
+
+# Clean the Image paths 
+regex_image_path = r'src="https:\/\/' + site + r'\.umich\.edu(.*?)"'
+substitution = r'src="\1"'
+modified_input = re.sub(regex_image_path, substitution, modified_input)
+
+# # Clean the Path (parents, any site with subdomain as part of the URL path)
+# regex_path = r'<Path>/parents(.*)</Path>'
+# substitution = r'<Path>\1</Path>'
+# modified_input = re.sub(regex_path, substitution, modified_input)
+
+#Write the modified output
+with open('out.xml', 'w') as file:
+    file.write(modified_input)
+
+print("Cleaned ", len(re.findall(regex, input)), " hrefs")
+
+print("Fixed ", len(re.findall(regex_image, input)), " images")
+
+print("Cleaned ", len(re.findall(regex_image_path, input)), " images paths")
