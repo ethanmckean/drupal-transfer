5ce46ba2f3f0e2b31e1e0dc5d743bc5cea765ec8
emckean <>
2023-07-05 10:31:29 -0400

    added btn-danger removal, need to refactor with options

--- script.py
+++ script.py
@@ -20,10 +20,19 @@ regex_image_path = r'src="https:\/\/' + site + r'\.umich\.edu(.*?)"'
 substitution = r'src="\1"'
 modified_input = re.sub(regex_image_path, substitution, modified_input)
 
-# # Clean the Path (parents, any site with subdomain as part of the URL path)
+# Clean the Path (parents, any site with subdomain as part of the URL path)
 # regex_path = r'<Path>/parents(.*)</Path>'
 # substitution = r'<Path>\1</Path>'
 # modified_input = re.sub(regex_path, substitution, modified_input)
+
+# Change button class (btn-danger deprecated in our instance of d9)
+regex_btn = r'<a class(.*)btn-danger'
+substitution = r'<a class\1btn-error'
+modified_input = re.sub(regex_btn, substitution, modified_input)
+
+print('# btn danger removed = ', len(re.findall(regex_btn, input)))
+regex_btn = r'btn-danger'
+print("# btn danger = ", len(re.findall(regex_btn, input)))
 
 #Write the modified output
 with open('out.xml', 'w') as file:
